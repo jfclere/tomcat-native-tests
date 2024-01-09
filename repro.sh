c3c179ae49068bf3a9cb5408fe13366f49b86d7e
Jean-Frederic Clere <jfclere@gmail.com>
2024-01-09 08:41:58 +0100

    Testing 10.1.18

--- repro.sh
+++ repro.sh
@@ -1,8 +1,14 @@
+TOMCAT_VERSION=10.1.18
+rm -rf ASF MY
 mkdir ASF
 mkdir MY
 
-(cd ASF; tar xf ../apache-tomcat-10.1.17.tar.gz)
-(cd MY; tar xf ../apache-tomcat-10.1.17-src/output/release/v10.1.17/bin/apache-tomcat-10.1.17.tar.gz)
+(cd apache-tomcat-${TOMCAT_VERSION}-src; ant release)
+
+echo "release rebuilt testing it"
+
+(cd ASF; tar xf ../apache-tomcat-${TOMCAT_VERSION}.tar.gz)
+(cd MY; tar xf ../apache-tomcat-${TOMCAT_VERSION}-src/output/release/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz)
 
 diff -ru ASF MY | grep -v jar
 if [ $? -eq 0 ]; then
@@ -12,6 +18,7 @@ fi
 
 for file in `(cd ASF; find . -name "*.jar")`
 do
+  echo "testing: $file"
   file=`echo $file | sed "s/.//"`
   rm -rf JARASF; mkdir JARASF
   rm -rf JARMY; mkdir JARMY
